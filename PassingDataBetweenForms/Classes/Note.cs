bb8dcd13a3769653da9686d529b573f59cb618b7
Karen Payne <Karen.1.Payne@oregon.gov>
2021-07-11 06:32:28 -0700

    added OnPropertyChanged(); for properties in Note class

--- PassingDataBetweenForms/Classes/Note.cs
+++ PassingDataBetweenForms/Classes/Note.cs
@@ -5,8 +5,29 @@ namespace PassingDataBetweenForms.Classes
 {
     public class Note : INotifyPropertyChanged
     {
-        public string Title { get; set; }
-        public string Content { get; set; }
+        private string _title;
+        private string _content;
+
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Content
+        {
+            get => _content;
+            set
+            {
+                _content = value;
+                OnPropertyChanged();
+            }
+        }
+
         public override string ToString() => Title;
         public event PropertyChangedEventHandler PropertyChanged;
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
