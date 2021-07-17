381a5aeff740995a7c6e89f9022c14cdfd0faee6
Karen Payne <Karen.1.Payne@oregon.gov>
2021-07-17 12:11:40 -0700

    refactoring

--- TimeLibrary/Classes/Helpers.cs
+++ TimeLibrary/Classes/Helpers.cs
@@ -3,35 +3,9 @@ using System;
 
 namespace TimeLibrary.Classes
 {
-    public class Helpers
+    public static class Helpers
     {
-        /// <summary>
-        /// Format elapsed time to a string
-        /// </summary>
-        /// <param name="years"></param>
-        /// <param name="months"></param>
-        /// <param name="days"></param>
-        /// <param name="hours"></param>
-        /// <param name="minutes"></param>
-        /// <param name="seconds"></param>
-        /// <param name="result"></param>
-        /// <returns></returns>
-        /// <remarks>
-        /// Not fully tested
-        /// </remarks>
-        public static string Elapsed(int years, int months, int days, int hours, int minutes, int seconds, string result)
-        {
-            result = years switch
-            {
-                0 when months > 0 => $"{months} months {days} days {hours} hours {minutes} minutes {seconds} seconds",
-                0 when months == 0 => $"{days} days {hours} hours {minutes} minutes {seconds} seconds",
-                0 when months > 0 => $"{months} months {days} days {hours} hours {minutes} minutes {seconds} seconds",
-                0 when months == 0 && days == 0 => $"{hours} hours {minutes} minutes {seconds} seconds",
-                _ => result
-            };
 
-            return result;
-        }
         /// <summary>
         /// Get elapsed time in years, months, days, hours, seconds
         /// </summary>
@@ -44,7 +18,7 @@ namespace TimeLibrary.Classes
         /// <param name="minutes"></param>
         /// <param name="seconds"></param>
         /// <param name="milliseconds"></param>
-        public static void GetElapsedTime(DateTime fromDate, DateTime toDate, out int years, out int months, out int days, out int hours, out int minutes, out int seconds, out int milliseconds)
+        public static void GetElapsedTime(this DateTime fromDate, DateTime toDate, out int years, out int months, out int days, out int hours, out int minutes, out int seconds, out int milliseconds)
         {
             // If from_date > to_date, switch them around.
             if (fromDate > toDate)
--- TimeLibraryConsoleApp/Program.cs
+++ TimeLibraryConsoleApp/Program.cs
@@ -7,21 +7,15 @@ namespace TimeLibraryConsoleApp
     {
         static void Main(string[] args)
         {
-            Helpers.GetElapsedTime(
-                new DateTime(DateTime.Now.Year,
-                    DateTime.Now.Month, DateTime.Now.AddDays(-12).Day, 2, 2, 0),
-                DateTime.Now,
-                out var years,
-                out var months,
-                out var days,
-                out var hours,
-                out var minutes,
-                out var seconds,
+            var fromDateTime = new DateTime(1956, 9, 24);
+            var toDateTime = DateTime.Now;
+
+            fromDateTime.GetElapsedTime(toDateTime, 
+                out var years, out var months, out var days,
+                out var hours, out var minutes, out var seconds, 
                 out _);
 
-            string result = "";
-            Console.WriteLine(Helpers.Elapsed(years, months, days, hours, minutes, seconds, result));
-
+            Console.WriteLine($"Age is {years} years {months} months {days} days");
             Console.ReadLine();
         }
     }
