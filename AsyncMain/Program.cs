5d95ac5c8c7214761f785529904c63282c4b28b1
Karen Payne <Karen.1.Payne@oregon.gov>
2021-07-29 08:19:14 -0700

    simplified code sample

--- AsyncMain/Program.cs
+++ AsyncMain/Program.cs
@@ -7,26 +7,19 @@ namespace AsyncMain
     {
         public static async Task<int> Main(string[] args)
         {
-            Console.Title = "async Task<int> Main";
-            int number1 = 5, number2 = 10;
-            Console.WriteLine($"Sum of {number1} and {number2} is: {await AdditionAsync(number1, number2)}");
+            static Task<string> Simple() => Task.Run(async () =>
+                {
+                    await Task.Delay(2000);
+                    return $"Hello from {nameof(Simple)}";
+                });
+
 
             Console.WriteLine(await Simple());
-            
-            Console.WriteLine("Press any key to exist.");
+
             Console.ReadKey();
             return 0;
         }
-        private static Task<int> AdditionAsync(int value1, int value2)
-        {
-            return Task.Run(() => SumIt(value1, value2));
 
-            int SumIt(int x, int y)
-            {
-                return x + y;
-            }
-        }
 
-        private static Task<string> Simple() => Task.Run(() => $"{nameof(Simple)}");
     }
 }
