b560e1cccc3d5eadaa2b0f9cb0c6fa329520d6c2
Karen Payne <Karen.1.Payne@oregon.gov>
2021-07-29 11:26:07 -0700

    temp code sample

---
+++ Labels/StringExtensions.vb
@@ -0,0 +1,11 @@
+﻿Public Module StringExtensions
+    <Runtime.CompilerServices.Extension>
+    Public Function ContainsInsensitive(source As String, toCheck As String) As Boolean
+        Return source.IndexOf(toCheck, StringComparison.OrdinalIgnoreCase) >= 0
+    End Function
+    <Runtime.CompilerServices.Extension>
+    Public Function ContainsAll(source As String, ParamArray values() As String) As Boolean
+        Return values.All(Function(x) source.ContainsInsensitive(x))
+    End Function
+
+End Module
