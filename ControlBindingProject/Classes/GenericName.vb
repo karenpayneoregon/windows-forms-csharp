a8e4afc9094f1b42ed22dc8c51fe398858c1eac3
Karen Payne <Karen.1.Payne@oregon.gov>
2021-08-20 06:57:28 -0700

    added class

---
+++ ControlBindingProject/Classes/GenericName.vb
@@ -0,0 +1,29 @@
+﻿Imports System.ComponentModel
+Imports System.Runtime.CompilerServices
+
+Namespace Classes
+    Public Class GenericName
+        Implements INotifyPropertyChanged
+
+        Private _name As String
+
+        Public Property Name() As String
+            Get
+                Return _name
+            End Get
+            Set
+                _name = Value
+                OnPropertyChanged()
+            End Set
+        End Property
+
+        Public Overrides Function ToString() As String
+            Return Name
+        End Function
+
+        Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+        Protected Overridable Sub OnPropertyChanged(<CallerMemberName> Optional ByVal propertyName As String = Nothing)
+            PropertyChangedEvent?.Invoke(Me, New PropertyChangedEventArgs(propertyName))
+        End Sub
+    End Class
+End NameSpace
