0efa052a44d757fb705dc4a023b3fe64903b5863
Karen Payne <Karen.1.Payne@oregon.gov>
2021-08-26 07:35:29 -0700

    temp project

--- InterpolationConsoleApp/Program.cs
+++ InterpolationConsoleApp/Program.cs
@@ -8,20 +8,13 @@ namespace InterpolationConsoleApp
     {
         static void Main(string[] args)
         {
-            foreach (var person in Mocking.People)
-            {
-                Console.WriteLine($"{person.Id:D3}{person, 12} - {person.Hobbies.Delimited()}");
-                Console.WriteLine();
-            }
-
-            Console.WriteLine();
 
             foreach (var person in Mocking.People)
             {
-                Console.WriteLine($"{person.Id:D2}{person,12}");
+                Console.WriteLine($"{person.Id,-5:D3}{person,-25}");
                 foreach (var hobby in person.Hobbies)
                 {
-                    Console.WriteLine($"\t{hobby.ID,-3}{hobby}");
+                    Console.WriteLine($"\t{hobby.ID:D2}{hobby,30}");
                 }
             }
 
@@ -31,8 +24,8 @@ namespace InterpolationConsoleApp
 
     public static class Extensions
     {
-        public static string Delimited(this List<Hobby> sender, string separater = "\t") 
-            => string.Join(separater, sender.Select(hobby => hobby.Name));
+        public static string Delimited(this List<Hobby> sender, string separator = "\t") 
+            => string.Join(separator, sender.Select(hobby => hobby.DisplayName));
     }
     public class Person
     {
@@ -47,30 +40,32 @@ namespace InterpolationConsoleApp
     public class Hobby
     {
         public int ID { get; set; }
-        public string Name { get; set; }
+        public string DisplayName { get; set; }
         public override string ToString() 
-            => $"{Name}";
+            => $"{DisplayName}";
     }
 
     public class Mocking
     {
         public static List<Hobby> HobbyList => new()
         {
-            new () {ID = 1, Name = "Gaming"},
-            new () {ID = 2, Name = "Music / Playing an instrument"},
-            new () {ID = 3, Name = "Sports"},
-            new () {ID = 4, Name = "Reading"},
-            new () {ID = 5, Name = "Fitness"},
-            new () {ID = 6, Name = "Crafting"},
-            new () {ID = 8, Name = "Blogging"},
-            new () {ID = 9, Name = "Cooking"},
-            new () {ID = 10, Name = "Dancing"},
-            new () {ID = 11, Name = "Pottery"}
+            new () {ID = 1, DisplayName = "Gaming"},
+            new () {ID = 2, DisplayName = "Music / Playing an instrument"},
+            new () {ID = 3, DisplayName = "Sports"},
+            new () {ID = 4, DisplayName = "Reading"},
+            new () {ID = 5, DisplayName = "Fitness"},
+            new () {ID = 6, DisplayName = "Crafting"},
+            new () {ID = 7, DisplayName = "Blogging"},
+            new () {ID = 8, DisplayName = "Cooking"},
+            new () {ID = 9, DisplayName = "Dancing"},
+            new () {ID = 10, DisplayName = "Camping"},
+            new () {ID = 11, DisplayName = "Pottery"},
+            new () {ID = 12, DisplayName = "Racing"}
         };
 
-        public static List<Person> People => new List<Person>()
+        public static List<Person> People => new()
         {
-            new Person()
+            new()
             {
                 Id = 1, 
                 FirstName = "Karen", 
@@ -83,10 +78,10 @@ namespace InterpolationConsoleApp
                     HobbyList[9]
                 }
             },
-            new Person()
+            new()
             {
                 Id = 2, 
-                FirstName = "Anne", 
+                FirstName = "Lisa", 
                 LastName = "Smith", 
                 Hobbies = new List<Hobby>()
                 {
@@ -94,7 +89,44 @@ namespace InterpolationConsoleApp
                     HobbyList[9], 
                     HobbyList[5]
                 }
+            },
+            new()
+            {
+                Id = 3,
+                FirstName = "Bill",
+                LastName = "Rickman",
+                Hobbies = new List<Hobby>()
+                {
+                    HobbyList[1],
+                    HobbyList[2],
+                    HobbyList[5]
+                }
+            },
+            new()
+            {
+                Id = 4,
+                FirstName = "Vince",
+                LastName = "Bushheit",
+                Hobbies = new List<Hobby>()
+                {
+                    HobbyList[10],
+                    HobbyList[2],
+                    HobbyList[1]
+                }
+            },
+            new()
+            {
+                Id = 5,
+                FirstName = "Bick",
+                LastName = "VU",
+                Hobbies = new List<Hobby>()
+                {
+                    HobbyList[5],
+                    HobbyList[6],
+                    HobbyList[8]
+                }
             }
+
         };
     }
 }
