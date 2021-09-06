863d2253f72af570058202996711c0d42d620280
Karen Payne <Karen.1.Payne@oregon.gov>
2021-09-06 11:01:30 -0700

    added id update

--- CustomersDemo/ListBoxSaveForm.cs
+++ CustomersDemo/ListBoxSaveForm.cs
@@ -34,6 +34,7 @@ namespace CustomersDemo
             if (!string.IsNullOrWhiteSpace(FirstNameTextBox.Text) && !string.IsNullOrWhiteSpace(LastNameTextBox.Text))
             {
                 _customersBindingList.Add(new Customer() { FirstName = FirstNameTextBox.Text, LastName = LastNameTextBox.Text });
+                Controls.OfType<TextBox>().ToList().ForEach(x => x.Text = "");
             }
         }
 
@@ -52,6 +53,12 @@ namespace CustomersDemo
         private void JsonSaveButton_Click(object sender, EventArgs e)
         {
             if (_customersBindingList.Count <= 0) return;
+
+            for (int index = 0; index < _customersBindingList.Count; index++)
+            {
+                _customersBindingList[index].CustomerIdentifier = index +1;
+            }
+            
             var (success, exception) = _customersBindingList.JsonToFile("CustomersSaved.json");
             MessageBox.Show(exception is null ? "Saved" : $"Failed\n{exception.Message}");
         }
