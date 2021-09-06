572aeb6a99a28ccfdbd88599962a28a061559145
Karen Payne <Karen.1.Payne@oregon.gov>
2021-09-06 09:18:08 -0700

    Timer

---
+++ CounterExample/MainForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace CounterExample
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblView = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TimesLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblView
+            // 
+            this.lblView.AutoSize = true;
+            this.lblView.Location = new System.Drawing.Point(95, 18);
+            this.lblView.Name = "lblView";
+            this.lblView.Size = new System.Drawing.Size(40, 13);
+            this.lblView.TabIndex = 0;
+            this.lblView.Text = "lblView";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Current";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Time executed";
+            // 
+            // TimesLabel
+            // 
+            this.TimesLabel.AutoSize = true;
+            this.TimesLabel.Location = new System.Drawing.Point(95, 44);
+            this.TimesLabel.Name = "TimesLabel";
+            this.TimesLabel.Size = new System.Drawing.Size(13, 13);
+            this.TimesLabel.TabIndex = 3;
+            this.TimesLabel.Text = "0";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 127);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(185, 33);
+            this.panel1.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CounterExample.Properties.Resources.AlF2;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 33);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(106, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "to exit";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(185, 160);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.TimesLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Count-up";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label TimesLabel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ CounterExample/MainForm.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.ComponentModel;
+using st = System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CounterExample
+{
+    public partial class MainForm : Form
+    {
+        st.Timer _counterTimer;
+        
+        public int _numberOfSecondsTrigger = 60;
+        public int _timesExecutes = 1;
+        private int _seconds = 0;
+        
+        public MainForm()
+        {
+            InitializeComponent();
+            
+            Shown += OnShown;
+            Closing += OnClosing;
+            
+        }
+
+        private void OnClosing(object sender, CancelEventArgs e)
+        {
+            _counterTimer.Dispose();
+        }
+
+        private void OnShown(object sender, EventArgs e)
+        {
+            StartTimer();
+        }
+
+        private void StartTimer()
+        {
+            _counterTimer = new st.Timer(Dispatcher);
+            _counterTimer.Change(1, st.Timeout.Infinite);
+        }
+
+        private async void Dispatcher(object state)
+        {
+            _counterTimer.Dispose();
+            await RunJob();
+            StartTimer();
+        }
+
+        private async Task RunJob()
+        {
+            _seconds++;
+
+            if ( _seconds > _numberOfSecondsTrigger )
+            {
+                _seconds = 0;
+                
+                TimesLabel.InvokeIfRequired(label => label.Text = _timesExecutes.ToString());
+                
+                _timesExecutes += 1;
+                
+                StartTimer();
+
+            }
+           
+            lblView.InvokeIfRequired( label => label.Text = TimeSpan.FromSeconds(_seconds).ToString("ss") );
+            
+            await Task.Delay(1000);
+        }
+       
+        
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData != (Keys.Alt | Keys.F2)) return base.ProcessCmdKey(ref msg, keyData);
+            Environment.Exit(0);
+            return true;
+
+        }
+
+    }
+}
