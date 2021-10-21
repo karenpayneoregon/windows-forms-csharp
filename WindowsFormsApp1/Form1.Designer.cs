bdafcebc589d594d61dde1d89f471cfc22f48160
Karen Payne <Karen.1.Payne@oregon.gov>
2021-10-21 11:11:56 -0700

    code sample

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Location = new System.Drawing.Point(280, 15);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(35, 13);
+            this.StatusLabel.TabIndex = 0;
+            this.StatusLabel.Text = "label1";
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(12, 12);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 1;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(93, 12);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(75, 23);
+            this.CancelButton.TabIndex = 2;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Location = new System.Drawing.Point(174, 12);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(100, 20);
+            this.TextBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 61);
+            this.Controls.Add(this.TextBox1);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.StatusLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Async demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label StatusLabel;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.TextBox TextBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private CancellationTokenSource _cts = new CancellationTokenSource();
+        public Form1()
+        {
+            InitializeComponent();
+            StatusLabel.Text = "";
+        }
+        private async Task AsyncMethod(IProgress<int> progress, CancellationToken ct)
+        {
+
+            for (int index = 100; index <= 120; index++)
+            {
+                //Simulate an async call that takes some time to complete
+                await Task.Delay(200);
+
+                if (ct.IsCancellationRequested)
+                {
+                    ct.ThrowIfCancellationRequested();
+                }
+
+                if (progress != null)
+                {
+                    progress.Report(index);
+                }
+
+            }
+
+        }
+
+        private async void StartButton_Click(object sender, EventArgs e)
+        {
+            var cancelled = false;
+            if (_cts.IsCancellationRequested == true)
+            {
+                _cts.Dispose();
+                _cts = new CancellationTokenSource();
+            }
+
+
+            var progressIndicator = new Progress<int>(ReportProgress);
+            try
+            {
+                await AsyncMethod(progressIndicator, _cts.Token);
+            }
+            catch (OperationCanceledException ex)
+            {
+                StatusLabel.Text = "Cancelled";
+                cancelled = true;
+            }
+
+            if (cancelled)
+            {
+                await Task.Delay(1000);
+                StatusLabel.Text = "Go again!";
+            }
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            _cts.Cancel();
+        }
+        private void ReportProgress(int value)
+        {
+            StatusLabel.Text = value.ToString();
+            TextBox1.Text = value.ToString();
+        }
+    }
+}
