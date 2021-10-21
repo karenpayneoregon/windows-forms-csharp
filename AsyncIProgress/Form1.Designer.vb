a0325b6e08f6c5ff39844076762cd14e6347d158
Karen Payne <Karen.1.Payne@oregon.gov>
2021-10-21 10:57:49 -0700

    new code sample

---
+++ AsyncIProgress/Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.StatusLabel = New System.Windows.Forms.Label()
+        Me.StartButton = New System.Windows.Forms.Button()
+        Me.CancellButton = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'StatusLabel
+        '
+        Me.StatusLabel.AutoSize = True
+        Me.StatusLabel.Location = New System.Drawing.Point(12, 9)
+        Me.StatusLabel.Name = "StatusLabel"
+        Me.StatusLabel.Size = New System.Drawing.Size(98, 13)
+        Me.StatusLabel.TabIndex = 0
+        Me.StatusLabel.Text = "Status place holder"
+        '
+        'StartButton
+        '
+        Me.StartButton.Location = New System.Drawing.Point(15, 39)
+        Me.StartButton.Name = "StartButton"
+        Me.StartButton.Size = New System.Drawing.Size(75, 23)
+        Me.StartButton.TabIndex = 1
+        Me.StartButton.Text = "Start"
+        Me.StartButton.UseVisualStyleBackColor = True
+        '
+        'CancellButton
+        '
+        Me.CancellButton.Location = New System.Drawing.Point(96, 39)
+        Me.CancellButton.Name = "CancellButton"
+        Me.CancellButton.Size = New System.Drawing.Size(75, 23)
+        Me.CancellButton.TabIndex = 2
+        Me.CancellButton.Text = "Cancel"
+        Me.CancellButton.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(13, 78)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(210, 121)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.CancellButton)
+        Me.Controls.Add(Me.StartButton)
+        Me.Controls.Add(Me.StatusLabel)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "IProgress example"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents StatusLabel As Label
+    Friend WithEvents StartButton As Button
+    Friend WithEvents CancellButton As Button
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ AsyncIProgress/Form1.vb
@@ -0,0 +1,60 @@
+﻿Imports System.Threading
+
+Public Class Form1
+    Private _cts As New CancellationTokenSource()
+    Public Sub New()
+        InitializeComponent()
+
+        StatusLabel.Text = ""
+
+    End Sub
+    Private Async Function AsyncMethod(progress As IProgress(Of Integer), ct As CancellationToken) As Task
+
+        For index As Integer = 100 To 120
+            'Simulate an async call that takes some time to complete
+            Await Task.Delay(200)
+
+            If ct.IsCancellationRequested Then
+                ct.ThrowIfCancellationRequested()
+            End If
+
+            If progress IsNot Nothing Then
+                progress.Report(index)
+            End If
+
+        Next
+
+    End Function
+
+    Private Async Sub StartButton_Click(sender As Object, e As EventArgs) Handles StartButton.Click
+
+        Dim cancelled = False
+        If _cts.IsCancellationRequested = True Then
+            _cts.Dispose()
+            _cts = New CancellationTokenSource()
+        End If
+
+
+        Dim progressIndicator = New Progress(Of Integer)(AddressOf ReportProgress)
+        Try
+            Await AsyncMethod(progressIndicator, _cts.Token)
+        Catch ex As OperationCanceledException
+            StatusLabel.Text = "Cancelled"
+            cancelled = True
+        End Try
+
+        If cancelled Then
+            Await Task.Delay(1000)
+            StatusLabel.Text = "Go again!"
+        End If
+
+    End Sub
+    Private Sub CancelButton_Click(sender As Object, e As EventArgs) Handles CancellButton.Click
+        _cts.Cancel()
+    End Sub
+    Private Sub ReportProgress(value As Integer)
+        StatusLabel.Text = value.ToString()
+        TextBox1.Text = value.ToString()
+    End Sub
+
+End Class
