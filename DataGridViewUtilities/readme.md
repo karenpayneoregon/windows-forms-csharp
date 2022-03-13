e81752262c96d20c4ae9047d1f3a76ccbe2925af
Karen Payne <paynekaren@comcast.net>
2022-03-13 04:29:02 -0700

    additions/updates

---
+++ DataGridViewUtilities/readme.md
@@ -0,0 +1,6 @@
+﻿# About
+
+Code sample for showing how to setup a class project to use with a windows form project.
+
+![image](assets/Versions.png)
+
---
+++ SqlServerAsyncReadCore/GridForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataGridViewUtilities.LanguageExtensions;
+using SqlServerAsyncReadCore.Classes;
+
+namespace SqlServerAsyncReadCore
+{
+    public partial class GridForm : Form
+    {
+        private readonly BindingSource bindingSource = new();
+        private readonly CancellationTokenSource _cancellationTokenSource = new();
+        public GridForm()
+        {
+            InitializeComponent();
+            Shown += OnShown;
+        }
+
+        private async void OnShown(object sender, EventArgs e)
+        {
+            bindingSource.DataSource = await DataOperations.ReadProductsTask(_cancellationTokenSource.Token);
+            dataGridView1.DataSource = bindingSource;
+            dataGridView1.ExpandColumns();
+
+        }
+    }
+}
